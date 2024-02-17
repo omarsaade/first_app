64dd0029419c6405c3f9546e34fd7883701020ae
omar saade <omarsaadeh11@gmail.com>
2024-02-17 12:55:52 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,24 @@ import 'package:flutter/material.dart';
 // MaterialApp,Scaffold,Color,Center...hol widget
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 121, 52, 241),
-        body: Center(
-          child: Text('Hello World!'),
+        // backgroundColor: Color.fromARGB(255, 121, 52, 241),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 88, 14, 216),
+                Color.fromARGB(255, 105, 132, 61),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Text(
+                style: TextStyle(fontWeight: FontWeight.bold), 'Hello World!'),
+          ),
         ),
       ),
     ),

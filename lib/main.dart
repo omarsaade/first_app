f98d22348f72dd5c0532879d601b8a93339c74fb
omar saade <omarsaadeh11@gmail.com>
2024-02-17 17:57:58 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 // MaterialApp,Scaffold,Color,Center...hol widget
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         // backgroundColor: Color.fromARGB(255, 121, 52, 241),
         body: GradientContainer(),
@@ -19,6 +19,11 @@ void main() {
 
 // class Gradient_Container  or like this ( he class mena widget w fi class li hye widget)
 class GradientContainer extends StatelessWidget {
+  // so el const hye men el optimization technique
+  // const hon : mishen mawdu3 el reuse wel memory w kaza
+  // construction functions
+  const GradientContainer({super.key});
+
   @override
   Widget build(context) {
     return Container(

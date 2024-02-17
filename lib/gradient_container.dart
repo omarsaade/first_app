982407fe8c13dcb85d3595c5beb22ddd48379b2e
omar saade <omarsaadeh11@gmail.com>
2024-02-17 18:05:22 +0200

    fix

---
+++ lib/gradient_container.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+// class Gradient_Container  or like this ( he class mena widget w fi class li hye widget)
+class GradientContainer extends StatelessWidget {
+  // so el const hye men el optimization technique
+  // const hon : mishen mawdu3 el reuse wel memory w kaza
+  // construction functions
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 88, 14, 216),
+            Color.fromARGB(255, 105, 132, 61),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 // MaterialApp,Scaffold,Color,Center...hol widget
@@ -12,41 +13,4 @@ void main() {
       ),
     ),
   );
-}
-
-// classes in flutter
-// building custom widgets
-
-// class Gradient_Container  or like this ( he class mena widget w fi class li hye widget)
-class GradientContainer extends StatelessWidget {
-  // so el const hye men el optimization technique
-  // const hon : mishen mawdu3 el reuse wel memory w kaza
-  // construction functions
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 88, 14, 216),
-            Color.fromARGB(255, 105, 132, 61),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 28,
-            color: Colors.white,
-          ),
-        ),
-      ),
-    );
-  }
 }

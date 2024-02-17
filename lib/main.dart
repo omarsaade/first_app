ab7ef827cc01aed6d366eaf529f8e07ad4c0343c
omar saade <omarsaadeh11@gmail.com>
2024-02-17 16:35:56 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,40 @@ void main() {
     MaterialApp(
       home: Scaffold(
         // backgroundColor: Color.fromARGB(255, 121, 52, 241),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 88, 14, 216),
-                Color.fromARGB(255, 105, 132, 61),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Text(
-                style: TextStyle(fontWeight: FontWeight.bold), 'Hello World!'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+// classes in flutter
+// building custom widgets
+
+// class Gradient_Container  or like this ( he class mena widget w fi class li hye widget)
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 88, 14, 216),
+            Color.fromARGB(255, 105, 132, 61),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
 }

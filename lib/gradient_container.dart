858699e17417cbbeada7871616bbf019e05a84dd
omar saade <omarsaadeh11@gmail.com>
2024-02-18 21:18:08 +0200

    fix

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -30,14 +30,28 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          // centered vertically
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-2.png',
               width: 300,
               height: 200,
             ),
+            const SizedBox(
+              height: 20,
+            ),
             TextButton(
               onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20,
+                // ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
               child: const Text('Roll Dice'),
             ),
           ],

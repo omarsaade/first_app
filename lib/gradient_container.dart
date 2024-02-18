f2aff1b0704336989885ffd10a1383b0d47bc724
omar saade <omarsaadeh11@gmail.com>
2024-02-18 20:05:00 +0200

    fix

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -23,8 +23,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello world'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
       ),
     );
   }
